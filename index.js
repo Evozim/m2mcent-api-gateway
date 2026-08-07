154a8910e99afd3d9050e3a0cc59a3c9b10dbd77
M2M Cent User <hi-ha@m2mcent.com>
2026-08-07 01:30:12 -0600

    fix: CORS OPTIONS handling and add contact email

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.use(express.json());
 // Enable CORS for x402scan
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
