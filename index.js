7729d6e0e70bf7b82e959bc12967899dd142d2e3
M2M Cent User <hi-ha@m2mcent.com>
2026-08-07 01:51:35 -0600

    fix: expose Payment-Required header for CORS

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Expose-Headers', 'Payment-Required');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
